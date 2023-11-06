cce4142e7280ff4c45e1f01280ff7983d71480f9
SengMitnick <sengmitnick@163.com>
2023-11-06 18:22:58 +0800

    update

--- setup.py
+++ setup.py
@@ -1,3 +1,29 @@
-from setuptools import setup
+import re
 
-setup()
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+    long_description = re.sub(r"\n!\[.*\]\(.*\)", "", long_description)
+    long_description = re.sub(r"\n- \[.*\]\(.*\)", "", long_description)
+
+setup(
+    name="code-ctags",
+    version=__version__,
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires='>=3.9',
+    entry_points={
+        "console_scripts": [
+            "code_ctags = ctags.main:main",
+        ],
+    },
+    description="output the labels of functions, classes, variables, etc. of all files currently downloaded to the terminal",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/paul-gauthier/aider",
+)
